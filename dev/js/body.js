6208f199def45f31941b53e761b4e493e90dc39e
cynthia renteria <crenteria@deviseis.com>
2018-04-25 14:43:20 -0700

    Agregando estructura inicial

---
+++ dev/js/body.js
@@ -0,0 +1,10 @@
+// Include body scripts in the right order
+// They will be concatenated using gulp-rigger in body.min.js
+
+//= vendor/jquery.js
+//= lib/jquery.navigation.js
+//= lib/settings.js
+//= lib/gmap.js
+
+// Google map disabled by default because of empty API key
+// It is still available at lib/gmap.js
